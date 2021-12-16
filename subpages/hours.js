a4f7f64b0155fcebb8d1fc76ec9d5e482a69ed75
HugoHsi <hsih550@newschool.edu>
2021-12-15 21:34:05 -0500

    Finished Styling
    Pretty Much Done

--- subpages/hours.js
+++ subpages/hours.js
@@ -1,40 +1,83 @@
-// get real time
-var time = new Date();
-//var hrs = time.getHours();
-var hrs = 23;
+function renderTime() {
+  // get real time
+  var time = new Date();
+  var hrs = time.getHours();
+  //var hrs = 23;
 
-// set array for hours in text format
-var hrsArray = new Array("Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen","Twenty","Twenty-One","Twenty-Two","Twenty-Three")
+  // set array for hours in text format
+  var hrsArray = new Array(
+    "Zero",
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+    "Eleven",
+    "Twelve",
+    "Thirteen",
+    "Fourteen",
+    "Fifteen",
+    "Sixteen",
+    "Seventeen",
+    "Eighteen",
+    "Nineteen",
+    "Twenty",
+    "Twenty-One",
+    "Twenty-Two",
+    "Twenty-Three"
+  );
 
-var hrsMinusTwo = hrs - 2
-var hrsMinusOne = hrs - 1
-var hrsPlusOne = hrs + 1
-var hrsPlusTwo = hrs + 2
+  var hrsMinusTwo = hrs - 2;
+  var hrsMinusOne = hrs - 1;
+  var hrsPlusOne = hrs + 1;
+  var hrsPlusTwo = hrs + 2;
 
-if(hrsMinusTwo < 0) {
-    hrsMinusTwo = hrs + 22
-};
+  if (hrsMinusTwo < 0) {
+    hrsMinusTwo = hrs + 22;
+  }
 
-//console.log(hrsMinusOne)
+  //console.log(hrsMinusOne)
 
-if(hrsMinusOne < 0) {
-    hrsMinusOne = hrs + 23
-};
+  if (hrsMinusOne < 0) {
+    hrsMinusOne = hrs + 23;
+  }
 
-if(hrsPlusOne > 23) {
-    hrsPlusOne = hrs - 23
-};
+  if (hrsPlusOne > 23) {
+    hrsPlusOne = hrs - 23;
+  }
 
-if(hrsPlusTwo > 23) {
-    hrsPlusTwo = hrs - 22
-};
+  if (hrsPlusTwo > 23) {
+    hrsPlusTwo = hrs - 22;
+  }
 
+  document.getElementById("1").innerHTML = hrsArray[hrsMinusTwo];
+  document.getElementById("2").innerHTML = hrsArray[hrsMinusOne];
+  document.getElementById("3").innerHTML = hrsArray[hrs];
+  document.getElementById("4").innerHTML = hrsArray[hrsPlusOne];
+  document.getElementById("5").innerHTML = hrsArray[hrsPlusTwo];
 
+  //var timeOfDay = (-Math.abs(12 - hrs) + 12)*0.084
 
+  var timeOfDay = Math.pow(Math.E, -Math.pow(hrs - 12, 2) / 5);
 
-document.getElementById("1").innerHTML = hrsArray[hrsMinusTwo];
-document.getElementById("2").innerHTML = hrsArray[hrsMinusOne];
-document.getElementById("3").innerHTML = hrsArray[hrs];
-document.getElementById("4").innerHTML = hrsArray[hrsPlusOne];
-document.getElementById("5").innerHTML = hrsArray[hrsPlusTwo];
+  console.log(timeOfDay);
+  //console.log(Math.E)
 
+  document.getElementById("background").style.opacity = timeOfDay;
+
+  if (timeOfDay < 0.5) {
+    document.getElementById("brightness").style.color = "white";
+    document.getElementById("title-btn").style.color = "white";
+    document.getElementById("hr-btn").style.color = "white";
+    document.getElementById("min-btn").style.color = "white";
+    document.getElementById("sec-btn").style.color = "white";
+    document.getElementById("semi-white").style.color = "white";
+  }
+}
+
+setInterval("renderTime()", 10000);
--- subpages/seconds.js
+++ subpages/seconds.js
@@ -1,34 +1,60 @@
-let loading = true
+let loading = true;
 
+const secondElement = document.querySelector(".second");
+const timeElement = document.querySelector(".time");
 
-const secondElement = document.querySelector(".second")
-const timeElement = document.querySelector(".time")
-
-
-
-function updateTime(){
+function updateTime() {
   const time = new Date();
-  
-  const second = time.getSeconds()
-  
-  if(!loading){
 
-secondElement.style.transition = `${second === 0 ? "none" : "all 1s ease-in"}`
-  }else{
-   
-secondElement.style.transition = `${second === 0 ? "none" : "all 0.3s ease-in"}`
+  const second = time.getSeconds();
+
+  if (!loading) {
+    secondElement.style.transition = `${
+      second === 0 ? "none" : "all 1s ease-in"
+    }`;
+  } else {
+    secondElement.style.transition = `${
+      second === 0 ? "none" : "all 0.3s ease-in"
+    }`;
   }
-  
- 
-  secondElement.style.transform = `translate(-50%,-100%) rotate(${scale(second,0,59,0,359)}deg)`
 
+  secondElement.style.transform = `translate(-50%,-100%) rotate(${scale(
+    second,
+    0,
+    59,
+    0,
+    359
+  )}deg)`;
 
-  loading = false
-  
+  loading = false;
 }
-function scale (number, inMin, inMax, outMin, outMax) {
-    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
 
+function updateRealTime() {
+  var time = new Date();
+  var realTime = time.getSeconds();
 
-setInterval(updateTime,100)
+  document.getElementById("num").innerHTML = realTime;
+}
+
+setInterval(updateTime, 100);
+setInterval(updateRealTime, 100);
+
+function toggle() {
+  if (document.getElementById("num").style.opacity == 0) {
+    console.log("1");
+    document.getElementById("num").style.opacity = 1;
+    console.log("2");
+    document.getElementById("clock").style.opacity = 0.25;
+    console.log("3");
+  } else {
+    document.getElementById("num").style.opacity = 0;
+    console.log("4");
+    document.getElementById("clock").style.opacity = 1;
+    console.log("5");
+  }
+}
+
+document.getElementById("clock").addEventListener("click", toggle);
